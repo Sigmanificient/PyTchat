a9ca5b1bf11e7f2b13a64d30bf22fd9766824ab5
Yohann Boniface <edhyjox@gmail.com>
2021-07-28 21:55:56 +0200

    using a class for client

--- client/main.py
+++ client/main.py
@@ -1,11 +1,13 @@
-import dotenv
 import errno
 import socket
 import sys
+import uuid
+
+import dotenv
+
+HEADER_LENGTH = 10
 
 print("Starting Client...")
-
-HEADER_LENGTH = 16
 
 config = dotenv.dotenv_values('.env')
 
@@ -20,41 +22,55 @@ if not PORT:
     quit()
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
+class Client:
 
-name = "foo".encode('utf-8')
-username_header = f"{len(name) : <{HEADER_LENGTH}}".encode("utf-8")
+    def __init__(self):
+        self.name = f"client {uuid.uuid4()}".encode("utf-8")
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client_socket.send(username_header + name)
+        self.socket.connect((IP, PORT))
+        self.socket.setblocking(False)
 
-while True:
-    message = input(">>> ").encode("utf-8")
-    message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
-    client_socket.send(message_header + message)
+        username_header = f"{len(self.name) : <{HEADER_LENGTH}}".encode("utf-8")
+        self.socket.send(username_header + self.name)
 
-    try:
-        username_header = client_socket.recv(HEADER_LENGTH)
+        self.send_message = ''
 
-        if not username_header:
-            print("Connection closed by the server")
+    def fetch_new_messages(self):
+        if len(self.send_message):
+            message = self.send_message.encode("utf-8")
+            message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
+            self.socket.send(message_header + message)
+            self.send_message = ''
+
+        new_messages = []
+
+        try:
+            while True:
+                username_header = self.socket.recv(HEADER_LENGTH)
+                if not len(username_header):
+                    print("Connection closed by the server")
+                    sys.exit()
+
+                username_length = int(username_header.decode("utf-8").strip())
+                username = self.socket.recv(username_length).decode("utf-8")
+
+                message_header = self.socket.recv(HEADER_LENGTH)
+                message_length = int(message_header.decode("utf-8").strip())
+                message = self.socket.recv(message_length).decode("utf-8")
+
+                new_messages.append({
+                    'author': username,
+                    'content': message
+                })
+
+        except IOError as e:
+            if e.errno not in [errno.EAGAIN, errno.EWOULDBLOCK]:
+                print("reading error", str(e))
+                sys.exit()
+
+            return new_messages
+
+        except Exception as e:
+            print("general error", str(e))
             sys.exit()
-
-        username_length = int(username_header.decode("utf-8").strip())
-        username = client_socket.recv(username_length).decode("utf-8")
-
-        message_header = client_socket.recv(HEADER_LENGTH)
-        message_length = int(message_header.decode("utf-8").strip())
-        message = client_socket.recv(message_length).decode("utf-8")
-
-        print(username, message)
-
-    except IOError as e:
-        if e.errno not in [errno.EAGAIN, errno.EWOULDBLOCK]:
-            print("reading error", str(e))
-            sys.exit()
-
-    except Exception as e:
-        print("general error", str(e))
-        sys.exit()
