1d696edfbfc19d17babb793b5c2709bbcd2a1151
Yohann Boniface <edhyjox@gmail.com>
2021-07-28 23:37:01 +0200

    reducing function complexity

--- client/src/client.py
+++ client/src/client.py
@@ -1,5 +1,4 @@
 import errno
-import socket
 import sys
 import uuid
 
@@ -8,42 +7,45 @@ HEADER_LENGTH = 10
 
 class Client:
 
-    def __init__(self, socket):
+    def __init__(self, client_socket):
         self.name = f"client {uuid.uuid4()}".encode("utf-8")
-        self.socket = socket
+        self.socket = client_socket
 
         username_header = f"{len(self.name) : <{HEADER_LENGTH}}".encode("utf-8")
         self.socket.send(username_header + self.name)
 
         self.send_message = ''
 
+    def send(self):
+        message = self.send_message.encode("utf-8")
+        message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
+        self.socket.send(message_header + message)
+        self.send_message = ''
+
+    def retrieve_next_message(self, new_messages):
+        username_header = self.socket.recv(HEADER_LENGTH)
+        if not len(username_header):
+            print("Connection closed by the server")
+            sys.exit()
+
+        username_length = int(username_header.decode("utf-8").strip())
+        username = self.socket.recv(username_length).decode("utf-8")
+
+        message_header = self.socket.recv(HEADER_LENGTH)
+        message_length = int(message_header.decode("utf-8").strip())
+        message = self.socket.recv(message_length).decode("utf-8")
+
+        new_messages.append({'author': username, 'content': message})
+
     def fetch_new_messages(self):
         if len(self.send_message):
-            message = self.send_message.encode("utf-8")
-            message_header = f"{len(message) :< {HEADER_LENGTH}}".encode("utf-8")
-            self.socket.send(message_header + message)
-            self.send_message = ''
+            self.send()
 
         new_messages = []
 
         try:
             while True:
-                username_header = self.socket.recv(HEADER_LENGTH)
-                if not len(username_header):
-                    print("Connection closed by the server")
-                    sys.exit()
-
-                username_length = int(username_header.decode("utf-8").strip())
-                username = self.socket.recv(username_length).decode("utf-8")
-
-                message_header = self.socket.recv(HEADER_LENGTH)
-                message_length = int(message_header.decode("utf-8").strip())
-                message = self.socket.recv(message_length).decode("utf-8")
-
-                new_messages.append({
-                    'author': username,
-                    'content': message
-                })
+                self.retrieve_next_message(new_messages)
 
         except IOError as e:
             if e.errno not in [errno.EAGAIN, errno.EWOULDBLOCK]:
--- server/src/core.py
+++ server/src/core.py
@@ -36,6 +36,66 @@ def start(ip, port):
         run_cycle(server_socket, socket_list, clients)
 
 
+def register_user_connection(server_socket, socket_list, clients):
+    client_socket, client_address = server_socket.accept()
+
+    user = receive_message(client_socket)
+
+    if not user:
+        return
+
+    socket_list.append(client_socket)
+    clients[client_socket] = user
+
+    print(
+        f"Accepted new connection from {client_address[0]}:"
+        f"{client_address[1]} username: {user['data'].decode('utf-8')}"
+    )
+
+
+def remove_user_connection(clients, notified_socket, socket_list):
+    print(
+        "Closed connection from",
+        clients[notified_socket]['data'].decode('utf-8')
+    )
+
+    socket_list.remove(notified_socket)
+    clients.pop(notified_socket)
+    return
+
+
+def send_message_to(client_socket, user, message):
+    client_socket.send(
+        user["header"] + user["data"] + message['header'] + message["data"]
+    )
+
+
+def broadcast_message(clients, notified_socket, user, message):
+    for client_socket in clients:
+        if client_socket != notified_socket:
+            send_message_to(client_socket, user, message)
+
+
+def handle_message(clients, notified_socket, message):
+    user = clients[notified_socket]
+    print(
+        f"receive_message from {user['data'].decode('utf-8')}:",
+        message['data'].decode('utf-8')
+    )
+
+    broadcast_message(clients, notified_socket, user, message)
+
+
+def handle_notified_socket(clients, notified_socket, socket_list):
+    message = receive_message(notified_socket)
+
+    if not message:
+        remove_user_connection(clients, notified_socket, socket_list)
+        return
+
+    handle_message(clients, notified_socket, message)
+
+
 def run_cycle(server_socket, socket_list, clients):
     read_sockets, _, exception_sockets = select.select(
         socket_list, [], socket_list
@@ -43,49 +103,11 @@ def run_cycle(server_socket, socket_list, clients):
 
     for notified_socket in read_sockets:
         if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
-
-            user = receive_message(client_socket)
-
-            if not user:
-                continue
-
-            socket_list.append(client_socket)
-            clients[client_socket] = user
-
-            print(
-                f"Accepted new connection from {client_address[0]}:"
-                f"{client_address[1]} username: {user['data'].decode('utf-8')}"
-            )
+            register_user_connection(server_socket, socket_list, clients)
 
         else:
-            message = receive_message(notified_socket)
-
-            if not message:
-                print(
-                    "Closed connection from",
-                    clients[notified_socket]['data'].decode('utf-8')
-                )
-
-                socket_list.remove(notified_socket)
-                del clients[notified_socket]
-                continue
-
-            user = clients[notified_socket]
-            print(
-                f"receive_message from {user['data'].decode('utf-8')}:",
-                message['data'].decode('utf-8')
-            )
-
-            for client_socket in clients:
-                if client_socket != notified_socket:
-                    client_socket.send(
-                        (
-                            user["header"] + user["data"]
-                            + message['header'] + message["data"]
-                        )
-                    )
+            handle_notified_socket(clients, notified_socket, socket_list)
 
     for notified_socket in exception_sockets:
         socket_list.remove(notified_socket)
-        del clients[notified_socket]
+        clients.pop(notified_socket)
